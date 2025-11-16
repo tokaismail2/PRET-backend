5474ce2244523791b2636e69b971d4658a51209b
tokaismail2 <tokaismaila@gmail.com>
2025-11-16 15:02:06 +0200

    .

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,18 @@
-export { default } from '../src/vercel';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import { AppModule } from '../src/app.module';
 
+const server = express();
+let isInitialized = false;
+
+export default async function handler(req: any, res: any) {
+	if (!isInitialized) {
+		const adapter = new ExpressAdapter(server);
+		const app = await NestFactory.create(AppModule, adapter);
+		await app.init();
+		isInitialized = true;
+	}
+	return (server as any)(req, res);
+}
 
