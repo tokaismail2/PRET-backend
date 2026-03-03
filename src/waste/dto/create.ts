8339d95fdf35f44b3151f71ca5b14907a25cf252
tokaismail2 <tokaismaila@gmail.com>
2026-03-03 16:57:04 +0200

    .

--- src/waste/dto/create.ts
+++ src/waste/dto/create.ts
@@ -1,12 +1,15 @@
-import { IsNumber, IsString, IsOptional, IsMongoId } from 'class-validator';
+import { IsNumber, IsString, IsOptional } from 'class-validator';
 import { Types } from 'mongoose';
+import { Transform } from 'class-transformer';
+import { IsMongoId } from 'class-validator';
 
 export class CreateWasteDto {
   @IsMongoId()
+  @Transform(({ value }) => new Types.ObjectId(value))
   warehouse_id: Types.ObjectId;
 
-
   @IsMongoId()
+  @Transform(({ value }) => new Types.ObjectId(value))
   material_id: Types.ObjectId;
 
 
