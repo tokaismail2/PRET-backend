ed864fc4cb14aa73f589e7f58abeac267ff07547
tokaismail2 <tokaismaila@gmail.com>
2026-03-03 17:13:21 +0200

    .

--- src/auction/dto/create.ts
+++ src/auction/dto/create.ts
@@ -1,20 +1,17 @@
 import { IsNumber, IsString, IsDate } from 'class-validator';
 import { Types } from 'mongoose';
-import { Transform } from 'class-transformer';
-import { IsMongoId } from 'class-validator';
 
 
 export class CreateAuctionDto {
 
-  @IsMongoId()
-  @Transform(({ value }) => new Types.ObjectId(value))
+  @IsString()
   waste_id: Types.ObjectId;
 
 
   @IsNumber()
   start_price: number;
 
-
+  
   @IsNumber()
   current_price: number;
 
