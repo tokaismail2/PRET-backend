6ffb0609e4ca1fae3860aa1b6b74c34389a1a9d8
tokaismail2 <tokaismaila@gmail.com>
2026-04-12 01:32:33 +0200

    updates

--- src/audit-log/audit-log.module.ts
+++ src/audit-log/audit-log.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuditLog, AuditLogSchema } from '../models/auditLog.schema';
 import { AuditLogController } from './dashboard.controller';
 import { AuditLogService } from './dashboard.service';
+import { User, UserSchema } from '../models/user.schema';
 
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: AuditLog.name, schema: AuditLogSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   exports: [MongooseModule],
